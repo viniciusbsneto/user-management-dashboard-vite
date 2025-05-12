4cfe5a615b5ccf86babf7c6bc51f90bc4bbe15f8
Vinícius Neto <viniciusbsneto@gmail.com>
2025-05-11 21:25:02 -0300

    feat: add web browser app routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Route, Routes } from 'react-router'
+
 import App from './App.tsx'
+import NotFound from './NotFound.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 )
