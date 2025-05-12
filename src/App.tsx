8a4092ab0b171d7c036fe8e7ffb593747b71f19f
Vinícius Neto <viniciusbsneto@gmail.com>
2025-05-11 23:31:04 -0300

    chore: setup Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import { BrowserRouter, Route, Routes } from 'react-router'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 
 import { NotFound, SignIn, SignUp, Users } from './pages'
 
 function App() {
+  const theme = createTheme()
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Users />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
   )
 }
 
