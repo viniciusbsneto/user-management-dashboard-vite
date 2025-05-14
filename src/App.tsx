dd37de334fc16b4edda18e6a7946cc6fd84e20d6
Vinícius Neto <viniciusbsneto@gmail.com>
2025-05-14 01:01:41 -0300

    refactor: setup tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 import { BrowserRouter } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { AuthProvider } from './contexts'
 import AppLayout from './layouts/AppLayout'
@@ -7,15 +8,18 @@ import Routes from './routes'
 
 function App() {
   const theme = createTheme()
+  const queryClient = new QueryClient()
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <BrowserRouter>
         <AuthProvider>
-          <AppLayout>
-            <Routes />
-          </AppLayout>
+          <QueryClientProvider client={queryClient}>
+            <AppLayout>
+              <Routes />
+            </AppLayout>
+          </QueryClientProvider>
         </AuthProvider>
       </BrowserRouter>
     </ThemeProvider>
