e10c6851b0642eba702620af7d004a6531bd614e
Vinícius Neto <viniciusbsneto@gmail.com>
2025-05-14 22:37:03 -0300

    chore: setup tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material'
 import { BrowserRouter } from 'react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 
 import { AuthProvider } from './contexts'
 import AppLayout from './layouts/AppLayout'
 import Routes from './routes'
 
+const STALE_TIME_1_MINUTE = 1000 * 60 * 1
+
 function App() {
   const theme = createTheme()
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient({
+    defaultOptions: { queries: { staleTime: STALE_TIME_1_MINUTE } },
+  })
 
   return (
     <ThemeProvider theme={theme}>
@@ -19,6 +24,7 @@ function App() {
             <AppLayout>
               <Routes />
             </AppLayout>
+            <ReactQueryDevtools initialIsOpen={false} />
           </QueryClientProvider>
         </AuthProvider>
       </BrowserRouter>
